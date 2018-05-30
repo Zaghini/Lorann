b4b839755534055dcfbbd49b2aba838e0241be78
drvnl <drvnl@LAPTOP-MUO3QSGU>
2018-05-30 17:06:03 +0200

    Voila

--- model/src/main/java/mobile/Mobile.java
+++ model/src/main/java/mobile/Mobile.java
@@ -1,13 +1,59 @@
 package mobile;
 
 public class Mobile {
+	
+	public int x;
+	public int y;
+	
+    
+    	public int getX() {
+    		return this.x;
+    		}
+        
+    	public void setX(int x) {
+    		this.x = x;
+        	}
+        
+    	public int getY() {
+    	        return this.y;
+    	    }
+        
+    	public void setY(int y) {
+    		this.y = y;
+    		}
+	
+	
+	this.setX(x);
+    this.setY(y);
+    
+	/**
+     * Move up.
+     */
+    public void moveUp() {
+        this.setY(this.getY() - 1);
+    }
 
-	public Mobile() {
-		// TODO Auto-generated constructor stub
-		
-		/*bonjour
-		 * 
-		 */
-	}
+    /**
+     * Move left.
+     */
+    public void moveLeft() {
+        this.setX(this.getX() - 1);
+    }
 
+    /**
+     * Move down.
+     */
+    public void moveDown() {
+        this.setY(this.getY() + 1);
+    }
+
+    /**
+     * Move right.
+     */
+    public void moveRight() {
+        this.setX(this.getX() + 1);
+    }
+	
+	
+	
 }
---
+++ model/src/main/java/mobile/Personnage.java
@@ -0,0 +1,9 @@
+package mobile;
+
+public class Personnage {
+
+	public Personnage() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
