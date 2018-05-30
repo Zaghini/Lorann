d945f2ea0970e63db8082d4696af51b482f34e8c
petri <petri@DESKTOP-VP5CQ2S>
2018-05-30 10:40:49 +0200

    commit

--- model/src/main/java/model/ModelFacade.java
+++ model/src/main/java/model/ModelFacade.java
@@ -13,38 +13,4 @@ import model.dao.ExampleDAO;
  */
 public final class ModelFacade implements IModel {
 
-    /**
-     * Instantiates a new model facade.
-     */
-    public ModelFacade() {
-        super();
-    }
-
-    /*
-     * (non-Javadoc)
-     * @see model.IModel#getExampleById(int)
-     */
-    @Override
-    public Example getExampleById(final int id) throws SQLException {
-        return ExampleDAO.getExampleById(id);
-    }
-
-    /*
-     * (non-Javadoc)
-     * @see model.IModel#getExampleByName(java.lang.String)
-     */
-    @Override
-    public Example getExampleByName(final String name) throws SQLException {
-        return ExampleDAO.getExampleByName(name);
-    }
-
-    /*
-     * (non-Javadoc)
-     * @see model.IModel#getAllExamples()
-     */
-    @Override
-    public List<Example> getAllExamples() throws SQLException {
-        return ExampleDAO.getAllExamples();
-    }
-
 }
