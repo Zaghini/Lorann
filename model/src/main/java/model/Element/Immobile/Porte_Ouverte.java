a2b0d56227c1661cf330ed8562d0c5f3a0b6430d
Maxime Ebe <38685696+TgaiaT@users.noreply.github.com>
2018-06-01 09:06:50 +0200

    Update Porte_Ouverte.java

--- model/src/main/java/model/Element/Immobile/Porte_Ouverte.java
+++ model/src/main/java/model/Element/Immobile/Porte_Ouverte.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 package model.Element.Immobile;
 
 import model.Element.Permeability;
@@ -12,18 +12,4 @@ public class Porte_Ouverte extends Immobile {
  	super(Porte_Ouverte,Permeability.PENETRABLE);
  	}
 }
-=======
-package model.Element.Immobile;
 
-import model.Element.Permeability;
-import model.Element.Sprite;
-
-public class Porte_Ouverte extends Immobile {
-	
- private static Sprite Porte_Ouverte = new Sprite('P',"gate_open.png");
- 
- 	Porte_Ouverte(){
- 	super(Porte_Ouverte,Permeability.PENETRABLE);
- 	}
-}
->>>>>>> origin/Petrinio
